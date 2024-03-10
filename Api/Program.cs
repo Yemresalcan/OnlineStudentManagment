d70ec58169894a752ac8487699f4a735f2713612
Yemresalcan <yunusemresalcan@gmail.com>
2024-03-10 21:29:50 +0300

    update

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,13 +5,17 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>
+(options=>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope()) { var services = scope.ServiceProvider; var dbContext = services.GetRequiredService<AppDbContext>(); dbContext.Database.Migrate(); }
 
 
 app.MapControllers();
